898e5599a39ce22caee67d117d9907b3c4931ca1
zahrirmdn <134955439+zahrirmdn@users.noreply.github.com>
2023-07-20 15:44:15 +0700

    upd reg

--- mylaravel-laravel/resources/views/register.blade.php
+++ mylaravel-laravel/resources/views/register.blade.php
@@ -6,24 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Halaman Regristasi</title>
     <link rel="stylesheet" href="gaya/style-registerasi.css">
-    <style>
-            body {
-            margin: 0;
-            padding: 0;
-            background-image: url('gambar/ukmsepak.jpeg');
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            background-position: center;
-        }
-    </style>
 </head>
 <body>
 
 <div class="container">
     <div class="row">
         <div class="col-md-5">
-            <div class="card card-header" style="background-color: cornflowerblue">Create New Account</div>
+            <div class="card card-header" style="background-color: whitesmoke">Create New Account</div><br>
             <div class="card-body">
                 <form action="{{ route('register') }}" method="post">
                     @csrf
